1b953efaa2aedea2802c3e66841f7050dec0ab77
snowWolf350 <pranet.v08@gmail.com>
2026-03-19 15:15:08 +0530

    Initial commit

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput Instance;
+
+    PlayerInput _playerInput;
+
+    bool _isSprinting;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.player.move.Enable();
+        _playerInput.player.sprint.Enable();
+    }
+
+    private void Start()
+    {
+        _playerInput.player.sprint.performed += Sprint_performed;
+        _playerInput.player.sprint.canceled += Sprint_canceled;
+    }
+
+    private void Sprint_canceled(InputAction.CallbackContext obj)
+    {
+        _isSprinting = false;
+    }
+
+    private void Sprint_performed(InputAction.CallbackContext obj)
+    {
+        _isSprinting = true;
+    }
+
+    public Vector3 GetInputVectorNormalized()
+    {
+        Vector2 MoveInputVector = _playerInput.player.move.ReadValue<Vector2>();
+
+        return new Vector3(MoveInputVector.x,0,MoveInputVector.y).normalized;
+    }
+
+    public bool PlayerIsSprinting()
+    {
+        return _isSprinting;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.Windows;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player")]
+    [Tooltip("Move speed of the character in m/s")]
+    public float MoveSpeed = 2.0f;
+
+    [Tooltip("Sprint speed of the character in m/s")]
+    public float SprintSpeed = 5.335f;
+
+    [Tooltip("How fast the character turns to face movement direction")]
+    [Range(0.0f, 0.3f)]
+    public float RotationSmoothTime = 0.12f;
+
+    [Tooltip("Acceleration and deceleration")]
+    public float SpeedChangeRate = 10.0f;
+
+    //player
+    private float _speed;
+    private float _animationBlend;
+    private float _targetRotation = 0.0f;
+    private float _rotationVelocity;
+    private float _verticalVelocity;
+    private float _terminalVelocity = 53.0f;
+    private bool rotateOnMove = true;
+
+    private CharacterController _controller;
+    Camera _mainCamera;
+
+    GameInput _gameInput;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _mainCamera = Camera.main;
+        _gameInput = GameInput.Instance;
+    }
+
+    private void Update()
+    {
+        Debug.Log(GameInput.Instance.GetInputVectorNormalized());
+        Move();
+    }
+
+    private void Move()
+    {
+        // set target speed based on move speed, sprint speed and if sprint is pressed
+        float targetSpeed = _gameInput.PlayerIsSprinting() ? SprintSpeed : MoveSpeed;
+
+        // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+        // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is no input, set the target speed to 0
+        if (_gameInput.GetInputVectorNormalized() == Vector3.zero) targetSpeed = 0.0f;
+
+        // a reference to the players current horizontal velocity
+        float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+
+        // accelerate or decelerate to target speed
+        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
+            currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            // creates curved result rather than a linear one giving a more organic speed change
+            // note T in Lerp is clamped, so we don't need to clamp our speed
+            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed,
+                Time.deltaTime * SpeedChangeRate);
+
+            // round speed to 3 decimal places
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
+        }
+        else
+        {
+            _speed = targetSpeed;
+        }
+
+        _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+        if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+        // normalise input direction
+        Vector3 inputDirection = _gameInput.GetInputVectorNormalized();
+
+        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is a move input rotate player when the player is moving
+        if (_gameInput.GetInputVectorNormalized() != Vector3.zero)
+        {
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                              _mainCamera.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                RotationSmoothTime);
+
+            // rotate to face input direction relative to camera position
+            if (rotateOnMove)
+            {
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
+        }
+
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+        // move the player
+        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
+                         new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+        // update animator if using character
+        //_animator.SetFloat(_animIDSpeed, _animationBlend);
+        //_animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+    }
+
+}
