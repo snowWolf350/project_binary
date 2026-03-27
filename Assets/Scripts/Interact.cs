938e2dda208fe3778a9e963d43d9fdd6d0d5fb77
snowWolf350 <pranet.v08@gmail.com>
2026-03-27 21:34:24 +0530

    made player jump to move to lvl 2

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Interact : MonoBehaviour
 {
@@ -8,12 +9,24 @@ public class Interact : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI interactText;
     [SerializeField] GameObject interactUI;
+    [SerializeField] Sprite _eButtonSprite;
+    [SerializeField] Image _eButtonImage;
+    public bool _usesInteraction;
 
-    bool lookAtPlayer;
+    public bool lookAtPlayer;
+    [Header("On Interaction")]
     public UnityEvent onInteraction;
+    [Header("On Enter")]
+    public UnityEvent OnEnter;
 
     private void Start()
     {
+        interactText.text = _interactText;
+        if (_usesInteraction)
+        {
+            _eButtonImage.sprite = _eButtonSprite;
+            _eButtonImage.enabled = true;
+        }
         Hide();
     }
     private void LateUpdate()
@@ -24,12 +37,15 @@ public class Interact : MonoBehaviour
     }
     public void Interacttion()
     {
+        if(_usesInteraction)
         onInteraction.Invoke();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         lookAtPlayer = true;
+        if(!_usesInteraction)
+        OnEnter.Invoke();
         Show();
         Player.Instance.SetCurrentInteractable(this);
     }
